24c6c4eef3f5b2671c91ba549167b6388476a0cc
007-JG <161293736+007-JG@users.noreply.github.com>
2026-04-08 13:55:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone', // Ye line Vercel ke liye helpful hoti hai
+}
+
 module.exports = nextConfig
